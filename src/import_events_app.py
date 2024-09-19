ac77c4ab0f568f935e754afc90bd5da8456b7472
Lee Roland <glroland@hotmail.com>
2024-09-19 17:12:07 -0400

    improved cli.  logging, linting, focusing console output

--- src/import_events_app.py
+++ src/import_events_app.py
@@ -1,28 +1,46 @@
 """ Baseball App CLI """
 import logging
+import sys
 import click
 from utils.db import truncate_table, connect_to_db
 from ingest.import_event_data import import_event_file
 
 @click.command()
-@click.option('--limit', '-l', default=-1, help='max games to import')
-@click.option('--truncate', '-t', default=True, is_flag=True, help='whether or not to truncate all the game tables before import')
-@click.option('--debug', '-d', default=False, is_flag=True, help='enable debug level logging or keep as info')
 @click.argument('event_file')
-def cli(limit, truncate, debug, event_file):
-    log_level = logging.INFO
-    if debug:
-        log_level = logging.DEBUG
-    logging.basicConfig(level=log_level,
-    handlers=[
-        # no need - logging.FileHandler("baseball-ingest.log"),
-        logging.StreamHandler()
-    ])
+@click.option('--limit', '-l', default=-1,
+              help='max games to import')
+@click.option('--truncate', '-t', default=True, is_flag=True,
+              help='whether or not to truncate all the game tables before import')
+@click.option('--debug', '-d', 'log_file', default=None,
+              help='enable debug level logging or keep as info')
+def cli(event_file, limit, truncate, log_file):
+    """ CLI utility for importing Retrosheet event files into the game
+        history database.  This tool assumes that team and roster data
+        has already been imported.
+    """
+    # Default to not set
+    logging.getLogger().setLevel(logging.NOTSET)
 
+    # Log info and higher to the console
+    console = logging.StreamHandler(sys.stdout)
+    console.setLevel(logging.INFO)
+    console.setFormatter(logging.Formatter('%(name)-13s: %(message)s'))
+    logging.getLogger().addHandler(console)
+
+    # Log debug and higher to a file
+    if log_file is not None:
+        file_handler = logging.FileHandler(log_file, 'w+')
+        file_handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+        logging.getLogger().addHandler(file_handler)
+
+    # (Optional) Truncate all game event data in the database
     if truncate:
         truncate_table(connect_to_db(), "game", True)
+
+    # Import event file
     import_event_file(event_file, limit)
 
-
 if __name__ == '__main__':
-    cli()
+    cli(None, None, None, None)
