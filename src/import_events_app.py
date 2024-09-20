d1fa9275f7b6d626ca4541adf5173e8167f91710
Lee Roland <glroland@hotmail.com>
2024-09-19 20:52:28 -0400

    import files or directories.  added missing truncate to makefile

--- src/import_events_app.py
+++ src/import_events_app.py
@@ -1,19 +1,20 @@
 """ Baseball App CLI """
 import logging
 import sys
+import os
 import click
 from utils.db import truncate_table, connect_to_db
-from ingest.import_event_data import import_event_file
+from ingest.import_event_data import import_event_file, import_all_event_data_files
 
 @click.command()
-@click.argument('event_file')
+@click.argument('event_file_or_dir')
 @click.option('--limit', '-l', default=-1,
               help='max games to import')
 @click.option('--truncate', '-t', default=True, is_flag=True,
               help='whether or not to truncate all the game tables before import')
 @click.option('--debug', '-d', 'log_file', default=None,
               help='enable debug level logging or keep as info')
-def cli(event_file, limit, truncate, log_file):
+def cli(event_file_or_dir, limit, truncate, log_file):
     """ CLI utility for importing Retrosheet event files into the game
         history database.  This tool assumes that team and roster data
         has already been imported.
@@ -39,8 +40,12 @@ def cli(event_file, limit, truncate, log_file):
     if truncate:
         truncate_table(connect_to_db(), "game", True)
 
-    # Import event file
-    import_event_file(event_file, limit)
+    if os.path.isfile(event_file_or_dir):
+        # Import event file
+        import_event_file(event_file_or_dir, limit)
+    elif os.path.isdir(event_file_or_dir):
+        # Import all files in directory
+        import_all_event_data_files(event_file_or_dir)
 
 if __name__ == '__main__':
     cli(None, None, None, None)
