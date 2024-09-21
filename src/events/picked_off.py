da60eeba354cf3dfdf5c3075983f194e2e82d0b6
Lee Roland <glroland@hotmail.com>
2024-09-21 16:39:48 -0400

    fixes

--- src/events/picked_off.py
+++ src/events/picked_off.py
@@ -3,6 +3,7 @@
 Runner picked off game event.
 """
 import logging
+import re
 from events.base_event import BaseEvent
 from utils.data import split_leading_num
 
@@ -16,21 +17,38 @@ class PickedOffEvent(BaseEvent):
         details_list = split_leading_num(details)
         logger.debug("Picked Off Base Details: %s", details_list)
         base = int(details_list.pop(0))
+
+        # see if there was an error during the pick off
+        override_po_due_to_error = False
+        credited_to = ""
+        if len(details_list) > 0:
+            d = details_list.pop(0)    
+            if re.match("^\(E[0-9]\)$", d):
+                override_po_due_to_error = True
+            elif re.match("^\([0-9]+\)$", d):
+                credited_to = d
+            else:
+                self.fail(f"Unknown picked off detail: {d}")
+
         logger.warning("Picked Off Details Being Ignored!: %s", details_list)
-        logger.info("Runnger Picked Off Base #%s", base)
-        game_at_bat.outs += 1
-        if base == 1:
-            # Not error checking this one because it can happen with a batter going to base
-            #if not game_at_bat.runner_on_1b:
-            #    self.fail("Encountered runner picked off event but no runner on first.")
-            game_at_bat.runner_on_1b = False
-        elif base == 2:
-            if not game_at_bat.runner_on_2b:
-                self.fail("Encountered runner picked off event but no runner on second.")
-            game_at_bat.runner_on_2b = False
-        elif base == 3:
-            if not game_at_bat.runner_on_3b:
-                self.fail("Encountered runner picked off event but no runner on third.")
-            game_at_bat.runner_on_3b = False
+
+        if override_po_due_to_error:
+            logger.info("Fielding team attempted to pick off runner but errored out.  Runner safe.")
         else:
-            self.fail(f"Unknown base encountered when processing runner picked off event: {base}")
+            logger.info("Runner Picked Off Base #%s by %s", base, credited_to)
+            game_at_bat.outs += 1
+            if base == 1:
+                # Not error checking this one because it can happen with a batter going to base
+                #if not game_at_bat.runner_on_1b:
+                #    self.fail("Encountered runner picked off event but no runner on first.")
+                game_at_bat.runner_on_1b = False
+            elif base == 2:
+                if not game_at_bat.runner_on_2b:
+                    self.fail("Encountered runner picked off event but no runner on second.")
+                game_at_bat.runner_on_2b = False
+            elif base == 3:
+                if not game_at_bat.runner_on_3b:
+                    self.fail("Encountered runner picked off event but no runner on third.")
+                game_at_bat.runner_on_3b = False
+            else:
+                self.fail(f"Unknown base encountered when processing runner picked off event: {base}")
