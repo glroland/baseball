4318266da7514eaf47f3117ca5d1bf1dbfec3180
Lee Roland <glroland@hotmail.com>
2024-09-21 17:19:22 -0400

    fixes

--- src/events/stolen_base.py
+++ src/events/stolen_base.py
@@ -4,6 +4,7 @@ Stolen base game event.
 """
 import logging
 from events.base_event import BaseEvent
+from utils.data import regex_split
 
 logger = logging.getLogger(__name__)
 
@@ -11,23 +12,29 @@ class StolenBaseEvent(BaseEvent):
     """ Stolen Base Event """
 
     def handle(self, game_at_bat, op_details):
-        base = op_details.pop(0)
-        if base == "2":
-            if not game_at_bat.runner_on_1b:
-                self.fail("Runner stealing 2nd but there was no runner on 1st")
-            game_at_bat.runner_on_1b = False
-            game_at_bat.runner_on_2b = True
-        elif base == "3":
-            if not game_at_bat.runner_on_2b:
-                self.fail("Runner stealing 3rd but there was no runner on 2nd")
-            game_at_bat.runner_on_2b = False
-            game_at_bat.runner_on_3b = True
-        elif base == "H":
-            if not game_at_bat.runner_on_3b:
-                self.fail("Runner stealing home but there was no runner on 3rd")
-            game_at_bat.runner_on_3b = False
-            game_at_bat.score()
-        else:
-            self.fail(f"Unknown Stolen Base # {base}")
+        details = op_details.pop(0)
+        base_list = regex_split("^([23H]);SB([23H])$", details)
 
-        logging.info("Runner stole base:", base)
+        logger.debug(f"Stolen Base List: {base_list}")
+        while len(base_list) > 0:
+            base = base_list.pop(0)
+
+            if base == "2":
+                if not game_at_bat.runner_on_1b:
+                    self.fail("Runner stealing 2nd but there was no runner on 1st")
+                game_at_bat.runner_on_1b = False
+                game_at_bat.runner_on_2b = True
+            elif base == "3":
+                if not game_at_bat.runner_on_2b:
+                    self.fail("Runner stealing 3rd but there was no runner on 2nd")
+                game_at_bat.runner_on_2b = False
+                game_at_bat.runner_on_3b = True
+            elif base == "H":
+                if not game_at_bat.runner_on_3b:
+                    self.fail("Runner stealing home but there was no runner on 3rd")
+                game_at_bat.runner_on_3b = False
+                game_at_bat.score()
+            else:
+                self.fail(f"Unknown Stolen Base # {base}")
+
+            logging.info("Runner stole base:", base)
