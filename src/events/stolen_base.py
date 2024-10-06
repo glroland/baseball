a96a54de0d68aff3dc4cd17c4d557d1d05178ead
Lee Roland <glroland@hotmail.com>
2024-10-05 22:30:29 -0400

    fixes

--- src/events/stolen_base.py
+++ src/events/stolen_base.py
@@ -7,12 +7,33 @@ from events.base_event import BaseEvent
 from utils.data import split_leading_chars_from_numbers
 from utils.baseball import validate_base
 from model.action_record import ActionRecord
+from model.advance_record import AdvanceRecord
 from model.game_state import GameState
 
 logger = logging.getLogger(__name__)
 
 class StolenBaseEvent(BaseEvent):
     """ Stolen Base Event """
+
+    def advance_if_not_already_handled(self, game_state, base_from, base_to):
+        """ Advance the runner, if this event has not already been handled
+            through an explicit advance
+            
+            game_state - game state
+            base_from - starting base
+            base_to - ending base
+        """
+        # was the advancement already handled?
+        advance = AdvanceRecord()
+        advance.base_from = base_from
+        advance.base_to = base_to
+        advance.was_out = False
+        if advance.is_completed(game_state._completed_advancements):
+            logger.debug("Stolen Base event already handled.")
+            return
+
+        # advance the runner
+        game_state.action_advance_runner(base_from, base_to, False)
 
     def handle(self, game_state : GameState, action : ActionRecord):
         """ Check the advancement history to see if this runner progression was
@@ -30,27 +51,9 @@ class StolenBaseEvent(BaseEvent):
         validate_base(base_to, first_allowed=False)
 
         # determine if the advancement was already completed
-        #if self.was_advancement_already_handled(base_to):
-        #    logger.warning("Advancement already handled.  Skipping!")
-        #elif base_to == "2":
-        if base_to == "2":
-            game_state.action_advance_runner("1", "2", False)
-        elif base_to == "3":
-            game_state.action_advance_runner("2", "3", False)
-        elif base_to == "H":
-            game_state.action_advance_runner("3", "H", False)
-
-        #for advance in self.completed_advances:
-        #    # get and validate base information from prior advances
-        #    old_from = advance[0]
-        #    self.validate_base(old_from)
-        #    old_to = advance[2]
-        #    self.validate_base(old_to)
-
-        #    if base_to == "2":
-        #        pass
-        #    if base_to == "3":
-        #        pass
-        #    if base_to == "H":
-        #        pass
-        #return False
+        if base_to in ["2", 2]:
+            self.advance_if_not_already_handled(game_state, "1", "2")
+        elif base_to in ["3", 3]:
+            self.advance_if_not_already_handled(game_state, "2", "3")
+        elif base_to in ["H", 4]:
+            self.advance_if_not_already_handled(game_state, "3", "H")
