1caa6ce9b776e09661795a97c958135dce84f2a6
Lee Roland <glroland@hotmail.com>
2024-10-06 13:53:36 -0400

    fixes

--- src/model/game_state.py
+++ src/model/game_state.py
@@ -343,6 +343,9 @@ class GameState(BaseModel):
                 # check for situation where advancement is to itself
                 if base_from == base_to and not out_override:
                     logger.info("Runner ran back to base.  Ignoring advancement...")
+                elif base_from == base_to and out_override:
+                    logger.info("Runner ran back to base but was still out!")
+                    self.on_out(base_from)
                 else:
                     # see if the advancement was already handled
                     if advance.is_completed(self._completed_advancements):
