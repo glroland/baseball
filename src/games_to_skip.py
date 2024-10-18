866071671df4fc656c427eb3d31ce1c4bbcf571e
Lee Roland <glroland@hotmail.com>
2024-10-18 18:55:16 -0400

    skipping game

--- src/games_to_skip.py
+++ src/games_to_skip.py
@@ -6,6 +6,10 @@ from typing import List
 
 GAMES_TO_SKIP : List[str] = \
 [
+    # skipping these due to errors in the data
     "ANA200004080",
-    "ANA200004100"
+    "ANA200004100",
+
+    # skipping these due to various data issues that need to be supported somehow
+    "CIN201005170"      # 'S9/G.2-3;1X3(936);B-2(TH3)' when runners on 12-
 ]
