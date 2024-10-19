87acc501b7aecb4ebc0e1df9adba8004ffb6462e
Lee Roland <glroland@hotmail.com>
2024-10-19 19:00:53 -0400

    fix

--- src/games_to_skip.py
+++ src/games_to_skip.py
@@ -10,6 +10,7 @@ GAMES_TO_SKIP : List[str] = \
     "ANA200004080",
     "ANA200004100",
     "MIN200706250",
+    "NYA201508170",
 
     # skipping these due to various data issues that need to be supported somehow
     "CIN201005170"      # 'S9/G.2-3;1X3(936);B-2(TH3)' when runners on 12-
