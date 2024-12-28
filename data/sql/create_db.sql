30119a2e6c2f0f919cfae4b51bab51d39b002f39
Lee Roland <glroland@hotmail.com>
2024-12-28 15:17:24 -0500

    feature store database

--- data/sql/create_db.sql
+++ data/sql/create_db.sql
@@ -1,4 +1,7 @@
 create user baseball_app with password 'baseball123';
 ALTER USER baseball_app WITH SUPERUSER;
-
 create database baseball_db with owner baseball_app;
+
+create user baseball_ml with password 'baseball123';
+ALTER USER baseball_ml WITH SUPERUSER;
+create database baseball_features_db with owner baseball_ml;
