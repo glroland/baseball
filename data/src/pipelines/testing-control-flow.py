bbf09ac284f29fc43e6918ec54cb4cc8c03b1d1e
Lee Roland <glroland@hotmail.com>
2025-04-01 19:27:47 -0400

    experimenting

---
+++ data/src/pipelines/testing-control-flow.py
@@ -0,0 +1,58 @@
+import subprocess
+import kfp
+import kfp.client
+from kfp import dsl, components
+from kfp.dsl import InputPath, Output, Artifact, Model
+from kfp import compiler
+
+@dsl.component
+def step1():
+    print ("STEP 1")
+
+@dsl.component
+def step2():
+    print ("STEP 2")
+
+@dsl.component
+def step3():
+    print ("STEP 3")
+
+@dsl.pipeline(name="Testing Control Flow Pipeline")
+def control_flow_pipeline():
+
+    # Step 1
+    step1_task = step1()
+    step1_task.set_display_name("step1")
+
+    # Step 2
+    step2_task = step2()
+    step2_task.set_display_name("step2")
+    step2_task.after(step1_task)
+
+    # Step 3
+    step3_task = step3()
+    step3_task.set_display_name("step3")
+    step3_task.after(step2_task)
+
+
+# Get OpenShift Token
+token = subprocess.check_output("oc whoami -t", shell=True, text=True).strip()
+
+# Connect to the pipeline server
+print ("Connecting to pipeline server")
+kfp_client = kfp.Client(host="https://ds-pipeline-dspa-baseball.apps.ocp.home.glroland.com/",
+                        existing_token=token,
+                        verify_ssl=False)
+
+# Create a run for the pipeline
+print ("Running Pipeline")
+kfp_client.create_run_from_pipeline_func(
+    control_flow_pipeline,
+    experiment_name="Testing Control Flow Pipeline v1",
+    arguments={
+    }
+)
+
+# Compile Pipeline
+print ("Compiling Pipeline")
+compiler.Compiler().compile(control_flow_pipeline, 'testing-control-flow.yaml')
